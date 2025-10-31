75ead19143dc180f2667f65bf3082aa61d6308d4
Precious Ogwezhi <pogwezhi@gmail.com>
2025-10-31 08:53:02 +0000

    initial commit

---
+++ convex/todos.ts
@@ -0,0 +1,36 @@
+import { v } from 'convex/values';
+import { mutation, query } from './_generated/server';
+
+export const getTodos = query({
+    handler: async (ctx) => {
+     return await ctx.db.query("todos").order("desc").collect()
+    }
+})
+
+export const addTodo = mutation({
+    args: {text: v.string()},
+    handler: async(ctx, args) => {
+        await ctx.db.insert("todos", {
+            text: args.text,
+            isCompleted: false,
+            createdAt: Date.now()
+        });
+    },
+});
+
+export const toggleTodos = mutation({
+  args: {id: v.id("todos")},
+  handler: async (ctx, args) => {
+    const todo = await ctx.db.get(args.id)
+    if(todo){
+        await ctx.db.patch(args.id, {isCompleted: !todo.isCompleted})
+    }
+  }
+})
+
+export const deleteTodo = mutation({
+    args: {id: v.id("todos")},
+    handler: async(ctx, args) => {
+        await ctx.db.delete(args.id)
+    }
+})
